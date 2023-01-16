fd82abc9f08035c31dde4ced1af36634e0136006
Alp Irez <99026213+alpirezz@users.noreply.github.com>
2023-01-16 12:10:06 +0300

    Update KeremSuper.py

--- KeremSuper.py
+++ KeremSuper.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.title("Kerem bu hafta Weekly'de 'Süper...' dedi mi?")
 answer = st.radio("Seçiniz:", ("Dedi", "Demedi"))
 
-if answer == "Dedi":
-    st.success("Doğru cevap! Kerem bu hafta Weekly'de 'Süper...' dedi.")
-    st.image("success.jpg") # path to your image file
+if answer == "Demedi":
+    st.success("Doğru cevap! Kerem bu hafta Weekly'de 'Süper...' demedi.")
+    st.image("success.jpg")
 else:
     st.error("Yanlış cevap! Kerem bu hafta Weekly'de 'Süper...' demedi.")
-    st.image("incorrect.jpg") # path to your image file
+    st.image("incorrect.jpg")
